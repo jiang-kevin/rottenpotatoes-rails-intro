78335ca350f9ed16ebc61286ac6fe62b53be1a32
Kevin Jiang <kjiang1998@gmail.com>
2020-01-04 16:19:06 +0000

    Part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort] == "title"
+      @movies = Movie.order(title: :asc)
+      @title_header = "hilite"
+    elsif params[:sort] == "date"
+      @movies = Movie.order(release_date: :asc)
+      @release_date_header = "hilite"
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
